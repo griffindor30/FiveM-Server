1360b0ea1ca47d82b6b56c678af9b730877763bb
Griffindor301 <griffin.hoover2@outlook.com>
2020-02-27 19:20:16 -0500

    Updated Lastpostdb

--- resources/[otherscripts]/lastpostodb/server.lua
+++ resources/[otherscripts]/lastpostodb/server.lua
@@ -1,45 +1,29 @@
---Version 1.5
---Déclaration des EventHandler
 RegisterServerEvent("projectEZ:savelastpos")
-RegisterServerEvent("projectEZ:SpawnPlayer")
-
---Intégration de la position dans MySQL
-AddEventHandler("projectEZ:savelastpos", function( LastPosX , LastPosY , LastPosZ , LastPosH )
+AddEventHandler("projectEZ:savelastpos", function(LastPosX , LastPosY , LastPosZ , LastPosH)
+	local source = source
 	TriggerEvent('es:getPlayerFromId', source, function(user)
-		--Récupération du SteamID.
-		local player = user.identifier
-		--Formatage des données en JSON pour intégration dans MySQL.
+		local player = user.getIdentifier()
 		local LastPos = "{" .. LastPosX .. ", " .. LastPosY .. ",  " .. LastPosZ .. ", " .. LastPosH .. "}"
-		--Exécution de la requêtes SQL.
-		MySQL.Async.execute("UPDATE users SET `lastpos`=@lastpos WHERE identifier = @username", {['@username'] = player, ['@lastpos'] = LastPos})
+		local executed_query = MySQL.Sync.execute("UPDATE users SET lastpos=@lastpos WHERE identifier=@username", {['@username'] = player, ['@lastpos'] = LastPos})
 	end)
 end)
 
+
+
+RegisterServerEvent("projectEZ:SpawnPlayer")
 AddEventHandler("projectEZ:SpawnPlayer", function()
+	local source = source
 	TriggerEvent('es:getPlayerFromId', source, function(user)
-		--Récupération du SteamID.
-		local player = user.identifier
-		--Récupération des données générée par la requête.
-		local result = MySQL.Sync.fetchScalar("SELECT lastpos FROM users WHERE identifier = @username", {['@username'] = player})	
-		-- Vérification de la présence d'un résultat avant de lancer le traitement.
-		if(result ~= nil)then
-				-- Décodage des données récupérées
-				local ToSpawnPos = json.decode(result)
-				print(ToSpawnPos[1])
-				-- On envoie la derniere position vers le client pour le spawn
-				TriggerClientEvent("projectEZ:spawnlaspos", source, ToSpawnPos[1], ToSpawnPos[2], ToSpawnPos[3])
+		local player = user.getIdentifier()
+		local result = MySQL.Sync.fetchAll("SELECT lastpos FROM users WHERE identifier=@username", {['@username'] = player})
+		-- local result = MySQL.Sync.fetchAll('SELECT identifier, lastpos FROM users')
+		if (result) then
+			for k,v in ipairs(result) do
+				if v.lastpos ~= "" then
+					local ToSpawnPos = json.decode(v.lastpos)
+					TriggerClientEvent("projectEZ:spawnlaspos", source, ToSpawnPos[1], ToSpawnPos[2], ToSpawnPos[3])
+				end
+			end
 		end
 	end)
-end)
-
--- Sauvegarde de la position lors de la deconnexion
-AddEventHandler('playerDropped', function()
-	TriggerEvent('es:getPlayerFromId', source, function(user)
-		--Récupération du SteamID.
-		local player = user.identifier
-		--Formatage des données en JSON pour intégration dans MySQL.
-		local LastPos = "{" .. user.coords.x .. ", " .. user.coords.y .. ",  " .. user.coords.z .. ", " .. user.coords.h .. "}"
-		--Exécution de la requêtes SQL.
-		MySQL.Async.execute("UPDATE users SET `lastpos`=@lastpos WHERE identifier = @username", {['@username'] = player, ['@lastpos'] = LastPos})
-  	end)
 end)
