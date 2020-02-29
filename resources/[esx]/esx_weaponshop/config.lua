2d49450fc87b23435170b45dff8f54fa9f9c2d18
Griffindor301 <griffin.hoover2@outlook.com>
2020-02-29 00:16:57 -0500

    Updated WeaponShop

--- resources/[esx]/esx_weaponshop/config.lua
+++ resources/[esx]/esx_weaponshop/config.lua
@@ -4,7 +4,8 @@ Config.DrawDistance  = 100
 Config.Size          = { x = 1.5, y = 1.5, z = 0.5 }
 Config.Color         = { r = 0, g = 128, b = 255 }
 Config.Type          = 1
-Config.Locale        = 'en'
+
+Config.Locale        = 'fr'
 
 Config.LicenseEnable = false -- only turn this on if you are using esx_license
 Config.LicensePrice  = 5000
@@ -13,13 +14,7 @@ Config.Zones = {
 
 	GunShop = {
 		Legal = true,
-		Items = {
-			{ weapon = 'WEAPON_APPISTOL', components = { 0, 0, 1000, 4000, nil }, price = 10000, ammoPrice = 20, AmmoToGive = 20 },
-			{ weapon = 'WEAPON_NIGHTSTICK', price = 500},
-			{ weapon = 'WEAPON_STUNGUN', price = 1500},
-			{ weapon = 'WEAPON_FLASHLIGHT', price = 80},
-			{ weapon = 'WEAPON_ADVANCEDRIFLE', price = 100, ammoPrice = 30, AmmoToGive = 20 }
-		},
+		Items = {},
 		Locations = {
 			vector3(-662.1, -935.3, 20.8),
 			vector3(810.2, -2157.3, 28.6),
@@ -35,11 +30,10 @@ Config.Zones = {
 
 	BlackWeashop = {
 		Legal = false,
-		Items = {
-			{ weapon = 'WEAPON_ADVANCEDRIFLE', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 50000, ammoPrice = 20, AmmoToGive = 20 }
-		},
+		Items = {},
 		Locations = {
 			vector3(-1306.2, -394.0, 35.6)
 		}
 	}
+
 }
